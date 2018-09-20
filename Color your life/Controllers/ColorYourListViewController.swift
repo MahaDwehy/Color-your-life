e88e30765cef8e0b42e677a98122061e6732abcc
Maha AlDwehy <mahadwehy@mahas-macbook-pro.local>
2018-09-20 11:40:47 +0300

    persisting data using Core Data.

--- Color your life/Controllers/ColorYourListViewController.swift
+++ Color your life/Controllers/ColorYourListViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
-
+import CoreData
 class ColorYourListViewController: UITableViewController {
     
     var itemArray = [Item]()
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    var selectedCategory : Category? {
+        didSet{
+            loadItems()
+        }
+    }
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(dataFilePath)
-        loadItems()
+      
+        print(FileManager.default.urls(for:.documentDirectory, in: .userDomainMask))
+    
         
     }
 
@@ -41,8 +49,11 @@ class ColorYourListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      
+        context.delete(itemArray[indexPath.row])
+        itemArray.remove(at: indexPath.row)
 
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         saveItems()
 
@@ -60,8 +71,11 @@ class ColorYourListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // What will happen once the user clicks the ass item button on our UIAlert
             
-            let newItem = Item()
+          
+            let newItem = Item(context: self.context)
             newItem.title = textField.text!
+            newItem.done = false
+            newItem.parentCategory = self.selectedCategory
             self.itemArray.append(newItem)
             self.saveItems()
         }
@@ -75,28 +89,53 @@ class ColorYourListViewController: UITableViewController {
     
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+       
         do {
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+      try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+        print("Error saving context(error)")
         }
         self.tableView.reloadData()
     }
     
-    func loadItems() {
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPresicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            let decoder = PropertyListDecoder()
-            do {
-            itemArray = try decoder.decode([Item].self, from: data)
-        } catch {
-            print("Error decoding item array, \(error)")
-      
+        if let addtionalPresicate = predicate{
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPresicate, addtionalPresicate])
+        } else {
+              request.predicate = categoryPresicate
         }
+      
+        do {
+           itemArray =  try  context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+
+    }
+    
+
+}
+// MARK: - Search bar methods
+extension ColorYourListViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+      let predicate = NSPredicate(format: "title CONTAINS %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+            searchBar.resignFirstResponder()
+            }
+    
         }
     }
 
 }
-
